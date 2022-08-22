b9cbbd4fdf007514343917c726020a736630a96c
Jeandre Putter <81087421+JJPxtter@users.noreply.github.com>
2022-08-22 17:31:25 +0200

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3f111c6f17msh697e0d18a1db2a2p1824d0jsn4f08d57bca33',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "3ee2a59c30842a9c80f21b31ec87fc5d"
